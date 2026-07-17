0114e5ad55baab8ce73789f05cb6f324836f33b6
Irarv <jhoanscitoacuna@gmail.com>
2026-07-17 00:32:22 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,68 +1,42 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import { 
-getDatabase,
-ref,
-push,
-set
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyBSou2aj8tlRMe0MlPuP87YgK0R0XW7R-A",
-
-authDomain: "carta-amor-d1cff.firebaseapp.com",
-
-databaseURL: "https://carta-amor-d1cff-default-rtdb.firebaseio.com",
-
-projectId: "carta-amor-d1cff",
-
-storageBucket: "carta-amor-d1cff.firebasestorage.app",
-
-messagingSenderId: "852809289289",
-
-appId: "1:852809289289:web:47601f5db9e2d900d97998",
-
-measurementId: "G-JCDCZHZQ81"
-
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  databaseURL: "https://TU_PROYECTO-default-rtdb.firebaseio.com",
+  projectId: "TU_PROYECTO",
 };
 
 
 const app = initializeApp(firebaseConfig);
-
-
 const db = getDatabase(app);
 
 
 
-window.guardarRespuesta = async function(respuesta){
+document.getElementById("guardar").addEventListener("click", ()=>{
 
 
-try{
+    let comentario = document.getElementById("comentario").value;
 
 
-const referencia = push(ref(db,"respuestas"));
+    const nuevo = push(ref(db,"comentarios"));
 
 
-await set(referencia,{
+    set(nuevo,{
+        texto: comentario,
+        fecha: new Date().toISOString()
+    })
+    .then(()=>{
+        console.log("Guardado correctamente");
+    })
+    .catch((error)=>{
+        console.log("ERROR:",error);
+    });
 
-respuesta:respuesta,
-
-fecha:new Date().toLocaleString()
 
 });
-
-
-console.log("Respuesta guardada");
-
-
-}catch(error){
-
-console.error(error);
-
-}
-
 
 }
 
